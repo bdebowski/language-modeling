6bd8b74c61c5a303a8156612e90eccfe4cb9ff81
Bazyli Debowski <bazyli.d@gmail.com>
2020-01-17 11:46:15 -0500

    first files commit

---
+++ predict_word_prog.py
@@ -0,0 +1,23 @@
+from word_predictor import WordPredictor
+
+
+class PredictWordProg:
+    def __init__(self):
+        self.text = ""
+
+    def run(self, m):
+        m.feed("")
+        while True:
+            self.display(m.top_n_next(10))
+            m.feed(self.read())
+
+    def display(self, top_n_next):
+        for w, p in top_n_next:
+            pass
+
+    def read(self):
+        return ""
+
+
+if __name__ == "__main__":
+    PredictWordProg().run(WordPredictor())
---
+++ word_predictor.py
@@ -0,0 +1,11 @@
+
+
+class WordPredictor:
+    def __init__(self):
+        self._m = None
+
+    def top_n_next(self, n):
+        pass
+
+    def feed(self, text):
+        pass
