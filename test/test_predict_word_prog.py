d445b96b7f1a4c5febc038cc049bfa94e9588039
bdebowski <bazyli.d@gmail.com>
2020-01-27 21:35:21 -0500

    created working text ui

---
+++ test/test_predict_word_prog.py
@@ -0,0 +1,7 @@
+from unittest import TestCase
+
+from predict_word_prog import PredictWordProg
+
+
+class TestPredictWordProg(TestCase):
+    pass
--- word_predictor.py
+++ word_predictor.py
@@ -5,7 +5,7 @@ class WordPredictor:
         self._m = None
 
     def top_n_next(self, n):
-        pass
+        return [("joe", 0.123), ("karen", 0.191), ("magic", 0.34)]
 
     def feed(self, text):
         pass
