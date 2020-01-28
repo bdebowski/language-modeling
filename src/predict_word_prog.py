506c843653440313b6820544206c47b5d06c9057
bdebowski <bazyli.d@gmail.com>
2020-01-28 15:59:27 -0500

    Implemented distilbert model for next word prediction. Model is not working as required.

--- src/predict_word_prog.py
+++ src/predict_word_prog.py
@@ -1,20 +1,25 @@
 import os
 
 from src.word_predictor import WordPredictor
+from src.model_factory import ModelName
 
 
 class PredictWordProg:
     def __init__(self):
-        self.text_history = []
+        self._text_history = ""
 
     def run(self, word_predictor):
+        """
+
+        :param word_predictor:
+        :return:
+        """
         word_predictor.feed("")
         while True:
             self.display(word_predictor.top_n_next(10))
-            tokens = self.read_user_input()
-            for t in tokens:
-                self.text_history.append(t)
-                word_predictor.feed(t)
+            text = self.read_user_input()
+            self._text_history += " " + text
+            word_predictor.feed(text)
 
     def display(self, top_n_next):
         """
@@ -22,27 +27,31 @@ class PredictWordProg:
         --------------------------------------------
         Last n words written are shown here followed by ______
 
-        predicted_next_0    score
-        predicted_next_1    score
-        predicted_next_2    score
-        predicted_next_3    score
-        predicted_next_4    score
+        score   predicted_next_0
+        score   predicted_next_1
+        score   predicted_next_2
+        score   predicted_next_3
+        score   predicted_next_4
 
         > user_input
-
         --------------------------------------------
+
+        :param top_n_next: list of 2-tuples (p, w) where w is a word and p is the likelihood of that word.
         """
         os.system("cls")
-        print("{} ____".format(" ".join(self.text_history[-20:])))
-        print("")
-        for w, p in top_n_next:
-            print("{}\t{:2f}".format(w, p))
+        if not self._text_history:
+            print("<Enter some text to get started...>")
+        else:
+            print("{} ____".format(self._text_history))
+            print("")
+            for p, w in top_n_next:
+                print("{:3f}\t{}".format(p, w))
         print("")
 
     @staticmethod
     def read_user_input():
-        return input("> ").split()
+        return input("> ")
 
 
 if __name__ == "__main__":
-    PredictWordProg().run(WordPredictor())
+    PredictWordProg().run(WordPredictor(ModelName.DISTILBERT))
